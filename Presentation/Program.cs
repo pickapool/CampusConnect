55ed7eed374ef445301ff8d3106f3a71a5dc4138
Cyrille Morales <cyrilleconm@gmail.com>
2025-10-04 15:15:43 +0800

    register service

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Presentation;
+using Presentation.Interfaces;
+using Presentation.Services.BaseService;
+using Presentation.Services.TokenProviderServices;
+using Presentation.Services.UserServices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,5 +16,10 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
+
 
 await builder.Build().RunAsync();
